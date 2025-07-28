882a559060b04df61cf8f678f62b5cb9c0a41730
vickash-zbb <vickashshivanzbb@gmail.com>
2025-07-29 04:20:17 +0530

    json and git change

--- vite.config.gh.ts
+++ vite.config.gh.ts
@@ -1,11 +1,12 @@
+// vite.config.gh.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/shopustore1/", // GitHub repo name
+  base: "/shopustore1/", // Match your GitHub repo name
   build: {
-    outDir: "dist",
+    outDir: "dist", // Must be 'dist' for GitHub Pages
   },
   plugins: [react()],
   resolve: {
